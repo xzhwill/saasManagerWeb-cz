c50196ce9f8295a30791a1f27b7e9276415cf655
xzhwill <m13581653749@139.com>
2020-09-23 17:31:29 +0800

    test1

--- src/pages/user/user.vue
+++ src/pages/user/user.vue
@@ -2,7 +2,7 @@
   <div>
     <section class="page-body">
       <transition name="fade">
-        <router-view class="page-body-info"></router-view> 
+        <router-view class="page-body-info"></router-view>
       </transition>
     </section>
   </div>
